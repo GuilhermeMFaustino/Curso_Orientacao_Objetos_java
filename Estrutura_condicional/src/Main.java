2d548146923c4c850c5ecfb06fa4ee266844ce05
Guilherme Mendes <guilhermemendes.info@gmail.com>
2025-11-27 20:55:58 -0300

    estrurura condicional simples

---
+++ Estrutura_condicional/src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println("Bom dia");
+
+        if( x > 0){
+            System.out.println("Boa Tarde");
+        }
+        System.out.println("Boa Noite");
+    }
+}
